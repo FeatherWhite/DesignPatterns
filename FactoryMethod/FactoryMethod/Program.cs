4b17fa91e8a93f2e862ffd49617a93dd3b73c227
FeatherWhite <tomw5823@outlook.com>
2021-06-18 22:09:59 +0800

    生成三个card对象

--- FactoryMethod/FactoryMethod/Program.cs
+++ FactoryMethod/FactoryMethod/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Framework;
+using IDCard;
 
 namespace FactoryMethod
 {
@@ -10,6 +12,13 @@ namespace FactoryMethod
     {
         static void Main(string[] args)
         {
+            Factory factory = new IDCardFactory();
+            Product card1 = factory.Create("小明");
+            Product card2= factory.Create("小红");
+            Product card3 = factory.Create("小刚");
+            card1.Use();
+            card2.Use();
+            card3.Use();
         }
     }
 }
