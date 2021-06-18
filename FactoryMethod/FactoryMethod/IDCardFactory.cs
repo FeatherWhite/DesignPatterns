211bd8005c8ef5209eff3244dbf4082122f8918b
FeatherWhite <tomw5823@outlook.com>
2021-06-18 21:57:33 +0800

    add IDCardFactory

---
+++ FactoryMethod/FactoryMethod/IDCardFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Framework;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace IDCard
+{
+    public class IDCardFactory : Factory
+    {
+        private ArrayList owners = new ArrayList();
+        protected override Product CreateProduct(string owner)
+        {
+            return new IDCard(owner);
+        }
+
+        protected override void RegisterProduct(Product product)
+        {
+            owners.Add(((IDCard)product).Owner);
+        }
+        public ArrayList Owners
+        {
+            get
+            {
+                return owners;
+            }
+        }
+    }
+}
