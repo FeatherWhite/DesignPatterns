8f149a5e8b9beff20413c2c4658e74de529459bd
FeatherWhite <tomw5823@outlook.com>
2021-06-18 21:57:16 +0800

    add IDCard

---
+++ FactoryMethod/FactoryMethod/IDCard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Framework;
+using System.Threading.Tasks;
+
+namespace IDCard
+{
+    public class IDCard : Product
+    {
+        private string owner;
+        public IDCard(string owner)
+        {
+            Console.WriteLine("制作"+owner+"的ID卡。");
+            this.owner = owner;
+        }
+        public override void Use()
+        {
+            Console.WriteLine("制作" + owner + "的ID卡。");
+        }
+        public string Owner
+        {
+            get
+            {
+                return owner;
+            }
+        }
+    }
+}
