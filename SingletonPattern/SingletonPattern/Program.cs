44b5531601acb587a3287b57dbbc8a01958c78eb
FeatherWhite <tomw5823@outlook.com>
2021-07-03 16:26:19 +0800

    对singleton进行引用

--- SingletonPattern/SingletonPattern/Program.cs
+++ SingletonPattern/SingletonPattern/Program.cs
@@ -6,7 +6,19 @@ namespace SingletonPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Singleton singleton1 = Singleton.GetInstance(0);
+            Console.WriteLine(singleton1.GetHashCode());
+            Singleton singleton2 = Singleton.GetInstance(1);
+            Console.WriteLine(singleton2.GetHashCode());
+            Singleton singleton3 = Singleton.GetInstance(2);
+            Console.WriteLine(singleton3.GetHashCode()); 
+            Singleton singleton4 = Singleton.GetInstance(0);
+            Console.WriteLine(singleton4.GetHashCode());
+            Singleton singleton5 = Singleton.GetInstance(1);
+            Console.WriteLine(singleton5.GetHashCode());
+            Singleton singleton6 = Singleton.GetInstance(2);
+            Console.WriteLine(singleton6.GetHashCode());
+            Console.ReadLine();
         }
     }
 }
