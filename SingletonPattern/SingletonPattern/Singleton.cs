d05e201e6b43dccedd063f0ddf369d29226ea254
FeatherWhite <tomw5823@outlook.com>
2021-07-03 16:24:45 +0800

    根据参数id生成不同的实例,最多可生成三个不同的实例

---
+++ SingletonPattern/SingletonPattern/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern
+{
+    public class Singleton
+    {
+        private static List<Singleton> singletons = new List<Singleton>();
+        public static Singleton GetInstance(int id)
+        {
+            if (id >= 0 && id < 3)
+            {
+                Singleton singleton = new Singleton();
+                singletons.Add(singleton);
+                return singletons[id];
+                //id++;
+                //code = id;
+            }
+            return singletons[0];
+        }
+    }
+}
